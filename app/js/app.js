23ffd29ba4d2e51184e6bc8b10c27a7b0985b61e
artemwebworking <artemwebworking@gmail.com>
2022-02-22 15:02:27 +0200

    tupotrash

--- app/js/app.js
+++ app/js/app.js
@@ -1,6 +1,7 @@
 import $ from 'jquery'
 window.jQuery = $
 window.$ = $
+import gsap from 'gsap'
 
 // // Import vendor jQuery plugin example (not module)
 // require('~/app/libs/mmenu/dist/mmenu.js')
@@ -8,5 +9,45 @@ window.$ = $
 document.addEventListener('DOMContentLoaded', () => {
 	
 	// Custom JS
-
+	TweenMax.staggerFrom(
+		".titles > div",
+		0.8,
+		{
+			x: "-60",
+			ease: Power3.easeInOut,
+			opacity: "0",
+		},
+		2
+	);
+	TweenMax.staggerTo(
+		".titles > div",
+		0.8,
+		{
+			x: "60",
+			ease: Power3.easeInOut,
+			delay: 1.2,
+			opacity: "0",
+		},
+		2
+	);
+	TweenMax.staggerFrom(
+		"li",
+		3,
+		{
+			x: "-1600",
+			delay: 4.2,
+			ease: Expo.easeInOut,
+		},
+		0.16
+	);
+	TweenMax.staggerTo(
+		"li",
+		2.6,
+		{
+			x: "1600",
+			delay: 6.8,
+			ease: Expo.easeInOut,
+		},
+		0.2
+	);
 })
--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,47 @@
+import gsap from 'gsap/dist/gsap'
+import gsap from 'gsap'
 document.addEventListener('DOMContentLoaded', function() {
-
+	TweenMax.staggerFrom(
+		".titles > div",
+		0.8,
+		{
+			x: "-60",
+			ease: Power3.easeInOut,
+			opacity: "0",
+		},
+		2
+	);
+	TweenMax.staggerTo(
+		".titles > div",
+		0.8,
+		{
+			x: "60",
+			ease: Power3.easeInOut,
+			delay: 1.2,
+			opacity: "0",
+		},
+		2
+	);
+	TweenMax.staggerFrom(
+		"li",
+		3,
+		{
+			x: "-1600",
+			delay: 4.2,
+			ease: Expo.easeInOut,
+		},
+		0.16
+	);
+	TweenMax.staggerTo(
+		"li",
+		2.6,
+		{
+			x: "1600",
+			delay: 6.8,
+			ease: Expo.easeInOut,
+		},
+		0.2
+	);
 	const showSlider = new Swiper('.showcase-carousel', {
 		loop: true,
 		slidesPerView: 3,
